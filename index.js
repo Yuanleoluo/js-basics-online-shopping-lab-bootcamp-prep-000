de285226b18eaae2c01541200f43fb11dbb4baeb
Yuanleoluo <leoyuanluo@gmail.com>
2017-10-27 23:12:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function viewCart() {
             console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]},
                     ${Object.keys(cart[1])} at $${cart[i][Object.keys(cart[1])]},
                     and ${Object.keys(cart[2])} at $${cart[i][Object.keys(cart[2])]}`)
-
+  }
 // write your code here
 }
 
