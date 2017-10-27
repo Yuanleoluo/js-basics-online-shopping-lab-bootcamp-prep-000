8be5614ec4671589651a7ecd970085d97cd11dc3
Yuanleoluo <leoyuanluo@gmail.com>
2017-10-27 22:48:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart = [];
+var cart = []
 
 function getCart() {
  return cart;
@@ -10,11 +10,31 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ cart.push({[item]: Math.floor((Math.random()*100)+1)})
+ console.log(`${item} has been added to your cart.`)
+ return cart// write your code here
 }
 
+
 function viewCart() {
-  // write your code here
+  if (cart.length === 0){
+    console.log("Your shopping cart is empty.")
+  } else {
+    var c = []
+    c.push("In your cart, you have")
+    for (let i=0; i<cart.length; i++){
+      if (cart.length===1) {
+        c.push(` ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}.`)
+      } else if (cart.length>2 && i===(cart.length-1)){
+        c.push(` and ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}.`)
+      } else {
+        c.push(` ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]},`)
+      }
+    }
+    console.log(c.join(''))
+  }
+
+// write your code here
 }
 
 function total() {
