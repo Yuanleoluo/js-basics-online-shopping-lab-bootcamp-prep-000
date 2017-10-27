538b4ed5b98bf1188140c5b0f7231b833bc4b674
Yuanleoluo <leoyuanluo@gmail.com>
2017-10-27 23:09:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,14 +20,14 @@ function viewCart() {
   if (cart.length === 0){
     console.log("Your shopping cart is empty.")
   } elseif (cart.length===1) {
-        console.log(`In your cart, you have ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}.`)
+        console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}.`)
   } elseif (cart.length===2){
-        console.log(`In your cart, you have ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}
-                    and ${Object.keys(cart[i+1])} at $${cart[i+1][Object.keys(cart[i+1])]}`)
+        console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}
+                    and ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}`)
   } else {
-        console.log(`In your cart, you have ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]},
-                ${Object.keys(cart[i+1])} at $${cart[i][Object.keys(cart[i+1])]},
-                and ${Object.keys(cart[i+2])} at $${cart[i][Object.keys(cart[i+2])]}`)
+        console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]},
+                ${Object.keys(cart[1])} at $${cart[i][Object.keys(cart[1])]},
+                and ${Object.keys(cart[2])} at $${cart[i][Object.keys(cart[2])]}`)
   }
 
 // write your code here
