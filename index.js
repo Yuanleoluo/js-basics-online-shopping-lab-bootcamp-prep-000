adfcd7382b338a8a6c45eafea5ac1f9e12061523
Yuanleoluo <leoyuanluo@gmail.com>
2017-10-27 23:15:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,8 +22,7 @@ function viewCart() {
   } else if (cart.length === 1) {
         console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}.`)
   } else if (cart.length===2){
-            console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}
-                        and ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}.`)
+            console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]} and ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}.`)
   } else {
             console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]},
                     ${Object.keys(cart[1])} at $${cart[i][Object.keys(cart[1])]},
