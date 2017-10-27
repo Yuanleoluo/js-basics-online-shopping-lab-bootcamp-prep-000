8ca1f1929a135a3a76fd274a2958cf70ea63a016
Yuanleoluo <leoyuanluo@gmail.com>
2017-10-27 23:10:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,13 +21,6 @@ function viewCart() {
     console.log("Your shopping cart is empty.")
   } elseif (cart.length===1) {
         console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}.`)
-  } elseif (cart.length===2){
-        console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}
-                    and ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}`)
-  } else {
-        console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]},
-                ${Object.keys(cart[1])} at $${cart[i][Object.keys(cart[1])]},
-                and ${Object.keys(cart[2])} at $${cart[i][Object.keys(cart[2])]}`)
   }
 
 // write your code here
