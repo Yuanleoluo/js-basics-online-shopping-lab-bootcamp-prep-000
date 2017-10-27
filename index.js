39b2a6df50849484e24ee0d8ab09d39f6f8b8995
Yuanleoluo <leoyuanluo@gmail.com>
2017-10-27 23:12:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ function viewCart() {
         console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}.`)
   } else if (cart.length===2){
             console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}
-                        and ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}`)
+                        and ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}.`)
   } else {
             console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]},
                     ${Object.keys(cart[1])} at $${cart[i][Object.keys(cart[1])]},
-                    and ${Object.keys(cart[2])} at $${cart[i][Object.keys(cart[2])]}`)
+                    and ${Object.keys(cart[2])} at $${cart[i][Object.keys(cart[2])]}.`)
   }
 // write your code here
 }
