084912cf4ac76b64beb0cc534728ee6b9e805ac1
Yuanleoluo <leoyuanluo@gmail.com>
2017-10-27 22:59:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,19 +20,17 @@ function viewCart() {
   if (cart.length === 0){
     console.log("Your shopping cart is empty.")
   } else {
-    var c = []
-    c.push("In your cart, you have")
+
     for (let i=0; i<cart.length; i++){
       if (cart.length===1) {
-        c.push(` ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}.`)
-      } else if (cart.length>2 && i===(cart.length-1)){
-        c.push(` and ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}.`)
+        console.log(`In your cart, you have ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}.`)
+      } else if (cart.length===2){
+        console.log(`In your cart, you have ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]} and ${Object.keys(cart[i+1])} at $${cart[i+1][Object.keys(cart[i+1])]}`)
       } else {
         c.push(` ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]},`)
       }
     }
-    process.stdout.write("hello: ")
-    process.stdout.write("my name is leo ")
+
     console.log(c.join(''))
   }
 
