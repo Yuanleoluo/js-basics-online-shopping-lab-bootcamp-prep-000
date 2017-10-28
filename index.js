285f4ce0b1a957b7a04d562d3d7f55a7259a464c
Yuanleoluo <leoyuanluo@gmail.com>
2017-10-28 00:10:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,8 +41,9 @@ function viewCart() {
 function total() {
   var sum = 0
   for (let i=0; i<cart.length; i++){
-    sum+= parseInt(cart[i][Object.keys(cart[i]))
+    sum += parseInt(cart[i][Object.keys(cart[i]))
   }
+  return sum
   // write your code here
 }
 
