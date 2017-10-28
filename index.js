63b7348db14e9652946eac99c26aae332cc4f36a
Yuanleoluo <leoyuanluo@gmail.com>
2017-10-28 00:17:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function total() {
 function removeFromCart(item) {
   for (let i=0; i<cart.length; i++){
     if (cart[i].hasOwnProperty(item)){
-      delete cart[i].item
+      delete cart.item
       return cart
     } else {
       console.log("That item is not in your cart.")
